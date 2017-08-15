6978da19919c7b3c26f8d1fd1ae0ae8d7e34526c
Claudiu Ghioc <claudiu.ghioc@gmail.com>
2017-08-15 21:08:21 +0300

    app: basic bottom navigation tab app
    Signed-off-by: Claudiu Ghioc <claudiu.ghioc@gmail.com>

---
+++ HartaBanilorPublici/app/src/main/java/initiativaromania/hartabanilorpublici/MapActivity.java
@@ -0,0 +1,49 @@
+package initiativaromania.hartabanilorpublici;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MapActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_map:
+                    mTextMessage.setText(R.string.title_map);
+                    return true;
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_search);
+                    return true;
+                case R.id.navigation_top:
+                    mTextMessage.setText(R.string.title_top);
+                    return true;
+                case R.id.navigation_info:
+                    mTextMessage.setText(R.string.title_info);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        BottomNavigationViewHelper.disableShiftMode(navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
